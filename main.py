915b3421b28b153a98e698346da09f5d62b4a6be
RickP2 <rnpri82@gmail.com>
2026-01-18 19:20:50 -0500

    At step 4 of data analysis section

---
+++ main.py
@@ -0,0 +1,19 @@
+def get_book_text(path_to_file):
+	
+	with open(path_to_file) as f:
+        # do something with f (the file) here
+		# f is a file object
+		file_contents = f.read()
+	return file_contents
+
+def word_count(book_selected):
+	words_in_work = book_selected.split()
+	num_words = len(words_in_work)
+	return num_words
+
+def main():
+	reading_book = get_book_text("books/frankenstein.txt")
+	words_thing = word_count(reading_book)
+	print(f"Found {words_thing} total words")
+
+main()
