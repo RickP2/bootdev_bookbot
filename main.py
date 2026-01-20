b6e48fe899d004309ab6e5a1ec78dd7510b2e721
RickP2 <rnpri82@gmail.com>
2026-01-19 21:07:47 -0500

    Working on printing the report

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from stats import word_count
-
+from stats import word_count, letter_count
 def get_book_text(path_to_file):
-	
-	with open(path_to_file) as f:
+    
+    with open(path_to_file) as f:
         # do something with f (the file) here
-		# f is a file object
-		file_contents = f.read()
-	return file_contents
+        # f is a file object
+        file_contents = f.read()
+    return file_contents
 
 
 
 def main():
-	reading_book = get_book_text("books/frankenstein.txt")
-	words_thing = word_count(reading_book)
-	print(f"Found {words_thing} total words")
+    reading_book = get_book_text("books/frankenstein.txt")
+    words_thing = word_count(reading_book)
+    words_thing_2 = letter_count(reading_book)
+    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n ----------- Word Count ----------\nFound {words_thing} total words\n --------- Character Count -------\n {words_thing_2}\n ============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,17 @@ def word_count(book_selected):
         words_in_work = book_selected.split()
         num_words = len(words_in_work)
         return num_words
+
+def letter_count(book_selected):
+    counts = {}
+
+    for letter_from_book in book_selected:
+        letter = letter_from_book.lower()
+        counts[letter] = counts.get(letter,0) + 1
+    def sort_counts(dict_key):
+        return dict_key[1]
+    
+    sorted_counts = sorted(counts.items(),reverse=True, key=sort_counts)
+    print(f"{counts}\n")
+
+
